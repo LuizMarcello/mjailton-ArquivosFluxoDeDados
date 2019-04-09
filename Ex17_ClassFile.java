2ecf60cf62643e0a48bcbe9544ab44ac478ca40e
LuizMarcello <luizmarcello.vmo@hotmail.com>
2019-04-09 16:48:13 -0300

    alterações

--- Ex17_ClassFile.java
+++ Ex17_ClassFile.java
@@ -8,6 +8,10 @@ public class Ex17_ClassFile{
         System.out.println("Nome do arquivo: " + arquivo.getName());
         System.out.println("Caminho do arquivo: " + arquivo.getAbsolutePath());
         System.out.println("O arquivo existe ? : " + arquivo.exists());
+        System.out.println("É um diretório ? : " + arquivo.isDirectory());
+        System.out.println("É um arquivo ? : " + arquivo.isFile());
+        System.out.println("Ele pode ser lido ? : " + arquivo.canRead());
+        System.out.println("Ele pode ser escrito ? : " + arquivo.canWrite());
         
 
     }
